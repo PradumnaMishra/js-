81ff952ba562cd77a5cc847106ff8b93f4794953
PradumnaMishra <118053843+PradumnaMishra@users.noreply.github.com>
2025-02-09 22:57:44 +0000

    datatype conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,16 @@
+let marks= undefined
+
+console.log(typeof marks)
+console.log(typeof(marks)) //as an object
+
+let valInNumber= Number(marks)
+console.log(valInNumber);
+console.log(typeof valInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// boolean --- true => 1, false => 0
+
+
