7d95ac1bee7402f67733d03ffbd4829e6ce6faa1
PradumnaMishra <118053843+PradumnaMishra@users.noreply.github.com>
2025-02-09 22:39:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treats all JS code as newer version
+
+alert(2 + 2) //would give error because it is node and not a browser's inspect element section
+
+//string => ""
+//number
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+
+//object
+
+console.log(typeof null) //would give object beacuse null is object
