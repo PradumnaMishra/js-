bfec91d456fef4b6132605dbd6f4cad898bb7a08
PradumnaMishra <118053843+PradumnaMishra@users.noreply.github.com>
2025-02-09 21:46:12 +0000

    studied variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const naam= "Pradumna"
+let age= "21"
+var empId="abcdef"
+empSalary= "50,000"
+// naam="Mishra"  //not allowed for constants
+console.log(naam);
+
+console.table([age, empId, empSalary])
+// var should be avoided beacuse it is not able to understand scope
