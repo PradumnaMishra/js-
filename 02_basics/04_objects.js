1df8a557401dd63f8097e072cf429a298988227d
PradumnaMishra <118053843+PradumnaMishra@users.noreply.github.com>
2025-03-31 22:23:06 +0000

    basic of objects done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,52 @@
+const obj= new Object() //non singleton way to declare an object
+const obj1={}
+obj1.email= "dopaminemishra@gmail.com"
+obj1.id= "abcx123"
+console.log(obj1);
+
+const obj2= {
+    name:{
+        fullName:{
+            firstName: "Pradumna",
+            secondName: "Mishra"
+        }
+
+    }
+}
+console.log(obj2.name.fullName.firstName);
+
+const obj3= {1: "a", 2: "b"}
+const obj4= {3: "c", 4: "d"}
+const obj5= Object.assign(obj3, obj4) //(target, source)
+console.log(obj3);
+console.log(obj5);
+//that's why in order to save these copied objects as new object, we add parenthesis()
+const obj6= Object.assign({}, obj3, obj4)
+ 
+//although we would use spread operator most oftenly
+// console.log(...obj3, ...obj4) //can't do this unlike arrays coz object is not iterable
+const obj7= {...obj3, ...obj4}
+
+console.log(Object.keys(obj1));
+console.log(Object.values(obj3));
+console.log(Object.entries(obj3));
+//all the above methods would return an array
+
+//to check if any key is present or not 
+console.log(obj2.hasOwnProperty("name"));
+
+//when we get any data from backend, it's in the form of array of objects
+const web=[{
+    id: "016969"
+},{
+    name: "lorem"
+}]
+console.log(web[1].name);
+
+
+
+
+
+
+
+
